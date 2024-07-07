f30838d990f1fe29b190a4ce3d8f964d1d175916
maheeen-khan <maheeenkhan4@gmail.com>
2024-07-07 19:11:23 +0500

    .

---
+++ app.js
@@ -0,0 +1,37 @@
+var myDate = new Date();
+var curr = myDate.getTime();
+document.write(curr);
+
+var ramadan = new Date("March 1, 2025");
+var nextRamadan = ramadan.getTime();
+document.write("<br>"+ nextRamadan);
+
+var differenceInMS = nextRamadan - curr;
+
+document.write("<br>"+ differenceInMS);
+
+var differenceInDays = differenceInMS / (1000 * 60 * 60 * 24) ;
+document.write(" <br><br>"+ Math.floor(differenceInDays)+ " days");
+
+// var differenceInDays = Math.floor(differenceInDays)  //+ " days";
+var a = differenceInDays - Math.floor(differenceInDays);
+document.write(" &nbsp"+ Math.floor(a*80) + " hours");
+
+var b = Math.floor(a*180) -  Math.floor(a*80);
+
+document.write(" &nbsp" + b  + " minutes");
+document.write(" &nbsp" + Math.floor(a*280) + " seconds");
+
+////////////Hours//////////////////
+
+var differenceforHours = new Date("March 1, 2025").getTime() - new Date().getTime();
+var hours = differenceforHours / (1000 * 60 * 60);
+var hours = Math.floor(hours) + " hours";
+
+document.write(" &nbsp&nbsp&nbsp&nbsp&nbsp"+ hours);
+
+///////////////Minutes///////////////
+var minutes = differenceforHours / (1000 * 60);
+var minutes = Math.floor(minutes) + " minutes";
+
+document.write(" &nbsp&nbsp&nbsp&nbsp&nbsp"+ minutes);
