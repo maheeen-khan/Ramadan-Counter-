089c7f4e52ffb9dc1578f6e8633520da11173ff5
maheeen-khan <maheeenkhan4@gmail.com>
2024-07-09 21:31:55 +0500

    updated

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ document.write("<br>"+ differenceInMS + " mili sec");
 var differenceInDays = differenceInMS / (1000 * 60 * 60 * 24) ;
 document.write(" <br><br>"+ Math.floor(differenceInDays)+ " days");
 
-// var differenceInDays = Math.floor(differenceInDays)  //+ " days";
 var a = (differenceInDays - Math.floor(differenceInDays) ) * 24; //gives hours in decimal form
 document.write(" &nbsp"+ Math.floor(a) + " hours");
 
@@ -26,18 +25,3 @@ var c = (b - Math.floor(b) ) * 60;
 document.write(" &nbsp" + Math.floor(c)  + " seconds");
 
 
-// document.write(" &nbsp" + Math.floor(a*280) + " seconds");
-
-////////////Hours//////////////////
-
-// var differenceforHours = new Date("March 1, 2025").getTime() - new Date().getTime();
-// var hours = differenceforHours / (1000 * 60 * 60);
-// var hours = Math.floor(hours) + " hours";
-
-// document.write(" &nbsp&nbsp&nbsp&nbsp&nbsp"+ hours);
-
-// ///////////////Minutes///////////////
-// var minutes = differenceforHours / (1000 * 60);
-// var minutes = Math.floor(minutes) + " minutes";
-
-// document.write(" &nbsp&nbsp&nbsp&nbsp&nbsp"+ minutes);
