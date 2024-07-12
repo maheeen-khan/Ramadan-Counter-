04d465f4129b17a84d3c44370b4164f1745f0511
maheeen-khan <maheeenkhan4@gmail.com>
2024-07-12 22:17:32 +0500

    updated

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var myDate = new Date();
 var curr = myDate.getTime();
-document.write(curr);
+// document.write(curr);
 
-var ramadan = new Date("February 28, 2025");
+var ramadan = new Date("February 28, 2025"); //set ramazan date
 var nextRamadan = ramadan.getTime();
-document.write("<br>"+ nextRamadan);
+// document.write("<br>"+ nextRamadan);
 
 var differenceInMS = nextRamadan - curr;
 
-document.write("<br>"+ differenceInMS + " mili sec");
+// document.write("<br>"+ differenceInMS + " mili sec");
 
 var differenceInDays = differenceInMS / (1000 * 60 * 60 * 24) ;
 document.write(" <br><br>"+ Math.floor(differenceInDays)+ " days");
@@ -24,4 +24,12 @@ var c = (b - Math.floor(b) ) * 60;
 
 document.write(" &nbsp" + Math.floor(c)  + " seconds");
 
+///////////////////////////////////////////////////////////
+var day =  document.getElementById('days');
+day.innerHTML = "<h2>" + Math.floor(differenceInDays) + "</h2>Days";
 
+var hour = document.getElementById('hours');
+hour.innerHTML = "<h2>" + Math.floor(a) + "</h2>Hours";
+
+var mins = document.getElementById('min');
+mins.innerHTML = "<h2>" + Math.floor(b) + "</h2>Minutes";
